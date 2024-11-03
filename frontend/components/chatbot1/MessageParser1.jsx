77d58d9d58243eaa4465811f99e07984a842ac1c
jingjietan <2000tanjingjie@gmail.com>
2024-11-04 00:39:32 +0800

    modified:   frontend/components/chatbot1/MessageParser1.jsx

--- frontend/components/chatbot1/MessageParser1.jsx
+++ frontend/components/chatbot1/MessageParser1.jsx
@@ -10,32 +10,32 @@ class MessageParser1 {
 
  async parse(message) {
     const lowercase = message.toLowerCase();
-    //I have submitted a past invoice sample.​
-    if (lowercase.includes("I have submitted a past invoice sample") ) {
+    //I submitted a past invoice sample.​
+    if (lowercase.includes("I submitted a past invoice sample".toLowerCase()) ) {
       this.actionProvider.handleQuestion1();
     }
-    //Okay, I need a system to track all the invoices I generated before. I have uploaded a list.​
-    else if (lowercase.includes(" I need a system to track all the invoices")) {
+    //Okay, I need a system to track all the invoices I generated before. I have uploaded a list.
+    else if (lowercase.includes("I need a system to track all the invoices I generated before".toLowerCase())) {
       this.actionProvider.handleQuestion2();
     }
     //Good, I want to know how many red color bottles I have sold.​
-    else if (lowercase.includes("yellow bottles")) {
+    else if (lowercase.includes("yellow bottles".toLowerCase())) {
       this.actionProvider.handleQuestion3();
     }
     //Okay, I want to create an invoice for MCMC for the purchase of 600 large bottles.​
-    else if (lowercase.includes("I want to create an invoice for MCMC")) {
+    else if (lowercase.includes("I want to create an invoice for MCMC".toLowerCase())) {
       this.actionProvider.handleQuestion4();
     }
     //Could you generate a report that tells me what is in high demand and what needs to be restocked?​
-    else if (lowercase.includes("you generate a report that tells me what is in high demand")) {
+    else if (lowercase.includes("you generate a report that tells me what is in high demand".toLowerCase())) {
       this.actionProvider.handleQuestion5();
     }
     //Here is the stock summary.
-    else if (lowercase.includes("Here is the stock summary")) {
+    else if (lowercase.includes("Here is the stock summary".toLowerCase())) {
       this.actionProvider.handleQuestion6();
     }
     //Could you briefly tell me which products users prefer? I would like a report on that.
-    else if (lowercase.includes("products users prefer")) {
+    else if (lowercase.includes("products users prefer".toLowerCase())) {
       this.actionProvider.handleQuestion7();
     }else{
      this.actionProvider.handleLoading();
